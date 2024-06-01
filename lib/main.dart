53ddb9d5abe2f9d79f92f03b59f05a2beb86f97e
K-taiga <taigakatou0329@gmail.com>
2024-06-01 17:23:31 +0900

    [add] ListView app

--- lib/main.dart
+++ lib/main.dart
@@ -30,81 +30,103 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _pushPage() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return NextPage(id: 1, name: 'プッシュ呼び出し');
-    }));
-  }
-
-  void _modalPage() {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) {
-            return NextPage(id: 2, name: 'モーダル呼び出し');
-          },
-          fullscreenDialog: true,
-        ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    _pushPage();
-                  },
-                  child: const Text('プッシュ遷移')),
-              ElevatedButton(
-                  onPressed: () {
-                    _modalPage();
-                  },
-                  child: const Text('モーダル遷移')),
-            ],
-          ),
-        ));
-    ;
-  }
-}
-
-class NextPage extends StatelessWidget {
-  // construct
-  NextPage({required this.id, required this.name});
-
-  final int id;
-  final String name;
+  List<Map<String, String>> contacts = [
+    {'name': '山田 太郎', 'number': '070-1234-5678', 'address': '東京都'},
+    {'name': '鈴木 一郎', 'number': '080-1234-5678', 'address': '神奈川'},
+    {'name': '佐藤 花子', 'number': '090-1234-5678', 'address': '大阪'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Next Page'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+                leading: const Icon(Icons.phone),
+                title: Text(contacts[index]['name']!),
+                subtitle: Text(contacts[index]['number']!),
+                trailing: const Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return DetailPage(contact: contacts[index]);
+                      },
+                    ),
+                  );
+                });
+          }),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key, required this.contact}) : super(key: key);
+
+  final Map<String, String> contact;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${contact['name']}'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Next Pageです。id = [$id] name = [$name]',
-              style: TextStyle(fontSize: 16),
+          child: Column(
+        children: [
+          const SizedBox(
+            width: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Icon(Icons.account_circle, size: 40),
+                Text('名前: ${contact['name']}',
+                    style: const TextStyle(fontSize: 20)),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(id == 1 ? '戻る' : '閉じる'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Icon(Icons.phone, size: 40),
+                Text('電話: ${contact['number']}',
+                    style: const TextStyle(fontSize: 20)),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Icon(Icons.home, size: 40),
+                Text('住所: ${contact['address']}',
+                    style: const TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+                onPressed: () {},
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.phone, size: 30),
+                    Text('電話をかける'),
+                  ],
+                )),
+          )
+        ],
+      )),
     );
   }
 }
