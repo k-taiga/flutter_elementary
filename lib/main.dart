45f063fc2198ce013011ea7e417dbf602d55082b
K-taiga <taigakatou0329@gmail.com>
2024-06-15 16:21:44 +0900

    [add] OpenWeatherMap App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -30,64 +31,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<List<String>> items = [
-    ['Flutter', 'https://flutter.dev/'],
-    ['Google', 'https://www.google.co.jp/'],
-    ['YouTube', 'https://www.youtube.com/'],
-  ];
+  TextEditingController controller = TextEditingController();
+  String areaName = '';
+  String weather = '';
+  double temperature = 0;
+  int humidity = 0;
+  double temperatureMax = 0;
+  double temperatureMin = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('ブックマーク')),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            final item = items[index];
-            final title = item[0] ?? '';
-            final url = item[1] ?? '';
-            return ListTile(
-                title: Text(title),
-                // onTapでWebViewPageに遷移
-                onTap: () {
-                  // contextとMaterialPageRouteを引数に渡す
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return WebViewPage(title: title, url: url);
-                  }));
-                });
-          },
-          itemCount: items.length,
-        ));
-  }
-}
+  Future<void> loadWeather(String query) async {
+    final response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?appid=810705e49e3ddf793ddc4bf31f996189&lang=ja&units=metric&q=$query'));
 
-class WebViewPage extends StatefulWidget {
-  const WebViewPage({required this.title, required this.url, super.key});
-  final String title;
-  final String url;
+    if (response.statusCode != 200) {
+      return;
+    }
 
-  @override
-  State<WebViewPage> createState() => _WebViewPageState();
-}
-
-class _WebViewPageState extends State<WebViewPage> {
-  // lateで宣言すると、初期化を遅らせることができる
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    // ここでWebViewControllerを初期化
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.url));
+    print(response.body);
+    final body = json.decode(response.body) as Map<String, dynamic>;
+    final main = (body['main'] ?? {}) as Map<String, dynamic>;
+    setState(() {
+      areaName = body['name'];
+      // weatherのなかの配列の0番目のdescriptionを取得 それぞれない場合を考慮し?でnull安全にしている
+      weather = (body['weather']?[0]?['description'] ?? '') as String;
+      humidity = (main['humidity'] ?? 0) as int;
+      temperature = (main['temp'] ?? 0) as double;
+      temperatureMax = (main['temp_max'] ?? 0) as double;
+      temperatureMin = (main['temp_min'] ?? 0) as double;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: WebViewWidget(
-          controller: controller,
-        ));
+      appBar: AppBar(
+          title: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          if (value.isNotEmpty) {
+            loadWeather(value);
+          }
+        },
+      )),
+      body: ListView(
+        children: [
+          ListTile(title: const Text('地域'), subtitle: Text(areaName)),
+          ListTile(title: const Text('天気'), subtitle: Text(weather)),
+          ListTile(
+              title: const Text('温度'), subtitle: Text(temperature.toString())),
+          ListTile(
+              title: const Text('最高温度'),
+              subtitle: Text(temperatureMax.toString())),
+          ListTile(
+              title: const Text('最低温度'),
+              subtitle: Text(temperatureMin.toString())),
+          ListTile(
+              title: const Text('湿度'), subtitle: Text(humidity.toString())),
+        ],
+      ),
+    );
   }
 }
